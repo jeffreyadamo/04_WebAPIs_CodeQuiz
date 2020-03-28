a5027a3197fbecc0315dc3986f1f115d6a46df05
jeffreyadamo <58490053+jeffreyadamo@users.noreply.github.com>
2020-03-28 16:52:13 -0700

    index++ bug persists; fixed if/else; working on Local Storage/high scores

--- script.js
+++ script.js
@@ -1,68 +1,68 @@
 // Set a few variables to begin
-var start = document.querySelector(".btn-lg");
+var start = document.querySelector(".startQuiz");
 var timerEl = document.querySelector(".timer");
 var timeRemaining = 60;
 var timerStart;
 var container = document.querySelector(".container");
 var buttons = document.querySelector(".buttons");
 var submit = document.querySelector(".submit");
-var index=0;
+var index = 0;
 var score = 0;
-var navEl = document.querySelector(".goAway");
-localStorage.setItem("score", score);
+var highScoresLinkEl = document.querySelector(".highScoresLink");
+
+// localStorage.setItem("score", score);
 
 // Set an array as a variable that includes questions, answers, and correct answers:
 var questionsArray = [
-    {
-        question: "Question 1: Commonly used data types DO NOT include:",
-        answers:
-           [ "1. strings",
-            "2. booleans",
-            "3. alerts", //correct
-            "4. numbers"],
-        correctOption: "3. alerts"
-    },
-    {
-        question: "Question 2: The condition of an if / else statement is enclosed within ________",
-        answers:
-          [ "1. quotes",
-           "2. curly brackets",
-           "3. parentheses", //correct
-           "4. square brackets"]
-        ,
-        correctOption: "3. parentheses"
-    },
-    {
-        question: "Question 3: Arrays in JavaScript can be used to store ________",
-        answers:
-          [ "1. numbers and strings",
-          "2. other arrays",
-          "3. booleans",
-          "4. all of the above"] //correct
-        ,
-        correctOption: "4. all of the above"
-    },
-    {
-        question: "Question 4: String values must be enclosed within ________ when being assigned to variables.",
-        answers:
-          [ "1. commas",
-          "2. curly brackets",
-          "3. quotes",
-          "4. parentheses"] //correct
-        ,
-        correctOption: "4. parentheses"
-    },
-    {
-        question: "Question 5: A very useful tool used during development and debugging for printing content to the debugger is:",
-        answers:
-          [ "1. JavaScript",
-          "2. terminal/bash",
-          "3. for loops", //correct
-          "4. console.log"]
-        ,
-        correctOption: "3. for loops"
-    }
-]
+  {
+    question: "Question 1: Commonly used data types DO NOT include:",
+    answers: [
+      "1. strings",
+      "2. booleans",
+      "3. alerts", //correct
+      "4. numbers"
+    ],
+    correctOption: "3. alerts"
+  },
+  {
+    question:
+      "Question 2: The condition of an if / else statement is enclosed within ________",
+    answers: [
+      "1. quotes",
+      "2. curly brackets",
+      "3. parentheses", //correct
+      "4. square brackets"
+    ],
+    correctOption: "3. parentheses"
+  },
+  {
+    question: "Question 3: Arrays in JavaScript can be used to store ________",
+    answers: [
+      "1. numbers and strings",
+      "2. other arrays",
+      "3. booleans",
+      "4. all of the above"
+    ], //correct
+    correctOption: "4. all of the above"
+  },
+  {
+    question:
+      "Question 4: String values must be enclosed within ________ when being assigned to variables.",
+    answers: ["1. commas", "2. curly brackets", "3. quotes", "4. parentheses"], //correct
+    correctOption: "4. parentheses"
+  },
+  {
+    question:
+      "Question 5: A very useful tool used during development and debugging for printing content to the debugger is:",
+    answers: [
+      "1. JavaScript",
+      "2. terminal/bash",
+      "3. for loops", //correct
+      "4. console.log"
+    ],
+    correctOption: "3. for loops"
+  }
+];
 
 // Make sure things are talking:
 console.log("Connected: not broken yet.");
@@ -70,113 +70,164 @@ console.log("Starting index is " + index);
 console.log("-------------");
 
 // Using addEventListener on "Start Quiz" button to start the timer:
-function startQuiz () {
-    start.addEventListener("click", function(event) {  
-         //prevent the button from functioning after timer begins
-        if(timeRemaining < 60) {
-            event.preventDefault();}
-       
-        else {
-         timerEl.textContent = "Time: " + timeRemaining; //Change text of button to 
-         console.log("Timer started; Time remaining: " + timeRemaining); //keep track of where we are at
-         timer(); //start function timer() which will countdown from timeRemaining
-         insertQuestion(); //insert questions starting at q1
-        }  
-    });
-} 
+function startQuiz() {
+  start.addEventListener("click", function(event) {
+    //prevent the button from functioning after timer begins
+    if (timeRemaining < 60) {
+      event.preventDefault();
+    } else {
+      timerEl.textContent = "Time: " + timeRemaining; //Change text of button to
+      console.log("Timer started; Time remaining: " + timeRemaining); //keep track of where we are at
+      console.log("-------------");
+
+      //Start function timer() which will countdown from timeRemaining:
+      timer();
+      //Insert questions starting at current index = 0:
+      insertQuestion();
+    }
+  });
+}
 
 // Use function timer() to countdown from timeRemaining:
 function timer() {
-    var timerInterval = setInterval(function() {
-      timeRemaining--;
-      timerEl.textContent = "Time: " + timeRemaining;
-       // Stop the timer once it reaches 0 and send a message.//
-      if(timeRemaining <= 0) {
-        clearInterval(timerInterval); 
-        timerEl.textContent = "Time's up!";
-        enterScore();
-        }
-    }, 1000); 
+  var timerInterval = setInterval(function() {
+    timeRemaining--;
+    timerEl.textContent = "Time: " + timeRemaining;
+
+    // Stop the timer once it reaches 0, send a message, and direct towards enterScore function:
+    if (timeRemaining <= 0) {
+      clearInterval(timerInterval);
+      timerEl.textContent = "Time's up!";
+      enterScore();
+    }
+  }, 1000);
 }
 
 //Now I need to prompt the user with a question.
 function insertQuestion() {
-    console.log("still working, insert question now"); //spot check to see if broken
-    console.log("Current index is " + index);
+  console.log("Still working,; running insertQuestion()"); //spot check to see if broken
+  console.log("Current index is " + index);
+  console.log("-------------");
 
-    // Use innerHTML to replace the contents of the container with a question. We want to use a different size font, so use a <h2> tag
-    // Access the first question in questionsArray using [index] which starts as [0]. We will index++ after each question cycle.
-    container.innerHTML = "<h2>" +questionsArray[index].question +"</h2>";
-    container.style.textAlign = "left";
+  // Use innerHTML to replace the contents of the container with a question. We want to use a different size font, so use a <h2> tag
+  // Access the first question in questionsArray using [index] which starts as [0]. We will index++ after each question cycle.
+  container.innerHTML = "<h2>" + questionsArray[index].question + "</h2>";
+  container.style.textAlign = "left";
 
-    console.log("Array length for answers is still:" + questionsArray[index].answers.length);    
-
-    // Create a for loop to populate the "buttons" <div> defined in the HTML with a button
-    for (var j = 0; j < questionsArray[index].answers.length; j++) {
-        var newDiv = document.createElement("div");
-        //new div created will have 2 classes, "answer[#]" and a style class = "questionStyle"//
-        newDiv.className =  "answer" + [j] + " questionStyle"; 
-        // Use innerHTML to insert a Bootstrap button pulling the text from the "questionsArray" listing the possible answers
-        newDiv.innerHTML = "<button type='button' class='btn btn-primary'>" + questionsArray[index].answers[j] +"</button>";
-        buttons.appendChild(newDiv);
-    };
-    
-    console.log("index after for loop is" + index);
-    console.log("questionsArray.length is" + questionsArray.length);
+//   Create a for loop to populate the answers in a button within their appropriate "answers" <div> defined in the HTML; children of the "buttons" <div>:
+  for (var j = 0; j < questionsArray[index].answers.length; j++) {
+    document.querySelector(".answer" + j).innerHTML = "<button type='button' class='btn btn-primary'>" + questionsArray[index].answers[j] + "</button>";
+  }
+//   document.querySelector(".answer0").innerHTML=questionsArray[index].answers[index]
+//   document.querySelector(".answer1").innerHTML=questionsArray[index].answers[index]
+//   document.querySelector(".answer2").innerHTML=questionsArray[index].answers[index]
+//   document.querySelector(".answer3").innerHTML=questionsArray[index].answers[index]
 
 
-    // only insert questions until questionsArray is complete.
-    if (index < questionsArray.length) {
-        correctAnswer();
-    // once all the answers are complete, we need to enter our score.
-    } else if (index = questionsArray.length) {
-        alert("game over");
-        enterScore;
-    }
+  console.log("index after for loop is " + index);
+  console.log("questionsArray.length is " + questionsArray.length);
+  console.log("-------------");
+
+  correctAnswer();
+
 }
 //Okay, now I have the first question. The next parts required are to 1) listen for the correct button answer 2) store the score 3) loop through series of questions 4)//
 
 function correctAnswer() {
-    buttons.addEventListener("click", function(event){
-        console.log(event.target.textContent);
-
-        if (event.target.textContent === questionsArray[index].correctOption) {
-            console.log("correct answer chosen");
-            localStorage.setItem("score", score + 10);
-  
-        } else {
-            console.log("incorrect answer");
-            timeRemaining = timeRemaining - 10
-            }
+  buttons.addEventListener("click", function(event) {
     event.preventDefault();
-    index++;
-    insertQuestion();
+    console.log("Button selected: " + event.target.textContent);
 
-    });
- 
-    return;
+    if (event.target.textContent === questionsArray[index].correctOption) {
+      console.log("correct answer chosen");
+      score = score + 10;
+      console.log("Currect score is: " + score);
+    } else {
+      console.log("incorrect answer");
+      timeRemaining = timeRemaining - 10;
+    }
+
+    if (index < questionsArray.length - 1) {
+        index++;
+        insertQuestion();
+        // once all the answers are complete, we need to enter our score.
+      } else {
+        enterScore;
+      }
+
+    
+    console.log("Index after answer is: " + index);
+    console.log("-------------");
+
+  });
+
+  return;
 }
 
 function enterScore() {
-    // document.parentNode.removeChild(navEl);
-    container.innerHTML = "<h2>All Done!</h2><br>";
-    buttons.innerHTML = "Your final score is " + localStorage.getItem("score") +"." + "<br><br>";
-    var form = document.createElement("form");
-    submit.appendChild(form);
-    form.innerHTML = enterInitials;
-    
+    // Replace quiz questions in container with a string 
+  container.innerHTML = "<h2>All Done!</h2><br>";
+//   Replace quiz answers with a string that shows cumulative score
+  buttons.innerHTML =
+    "Your final score is " + score + "." + "<br><br>";
+
+// create a form that allows the user to enter their initials
+  var form = document.createElement("form");
+  submit.appendChild(form);
+
+//  Insert a Bootstap form that is defined as "enterInitials" (defined at bottom) in the empty <form> element predefined in HTML.
+  form.innerHTML = enterInitials;
+// variable set to identify where the Submit button is
+  var initialsButton=document.querySelector(".initialButton")
+
+
+  
+    //   define what the user enters into the form
+      var userInitials=document.querySelector(".initInput").value
+      console.log(userInitials); //spot check
+    //Store the variables into an array
+      var userScore=[
+          userInitials,
+          score
+      ]
+      //Stringify!
+      localStorage.setItem("user,score", JSON.stringify(userScore))
+    //   When clicking submit
+      initialsButton.addEventListener("click",function(event){
+      event.preventDefault() //make sure not to refresh
+      viewScore();
+
+  })
 }
 
-function viewScore(){
-    
+function viewScore() {
+  container.innerHTML = "<h2>High Scores!</h2><br>";
+  var scoreList = document.createElement("div");
+  scoreList.textContent = "Scores go here";
+  container.appendChild(scoreList);
+
+  userInitials.innerHTML = JSON.parse(localStorage.getItem("user,score"));
+  
+
+//   var newButtons = document.createElement("div");
+//   newButtons.className = "newButtons";
+//   (newButtons.innerHTML =
+//     "<button type='button' class='btn btn-primary'>Go Back</button>"),
+//     "<button type='button' class='btn btn-primary'>Go Back</button>";
+//   buttons.appendChild(newButtons);
 }
+
+highScoresLinkEl.addEventListener("click", function(event) {
+  event.preventDefault();
+  viewScore();
+});
 
 // var score = localStorage.getItem("score");
 
 startQuiz();
 
-
 // part 4) Entering initials into final score and being able to retrieve them after refresh
 
-var enterInitials = 
-    '<form> <div class="form-row align-items-center"> <div class="col-md-6 my-1"> <label class="sr-only" for="inlineFormInputName">Name</label> <input type="text" class="form-control" id="inlineFormInputName" placeholder="Enter Initals"> </div> <div class="col-auto my-1"> </div> <div class="col-auto my-1"> <button type="submit" class="btn btn-primary">Submit</button> </div> </div> </form>'
+var enterInitials =
+  '<form> <div class="form-row align-items-center"> <div class="col-md-6 my-1"> <label class="sr-only" for="inlineFormInputName">Name</label> <input type="text" class="form-control initInput" id="inlineFormInputName" placeholder="Enter Initals"> </div> <div class="col-auto my-1"> </div> <div class="col-auto my-1"> <button type="submit" class="btn btn-primary initialButton">Submit</button> </div> </div> </form>';
+
